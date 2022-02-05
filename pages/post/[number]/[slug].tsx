b4ca83c37685f199265caaf5412099b993e85e8d
sdennett55@gmail.com <sdennett55@gmail.com>
2022-02-04 22:08:03 -0500

    used markdown files to make the dynamic blog post

--- pages/post/[number]/[slug].tsx
+++ pages/post/[number]/[slug].tsx
@@ -1,17 +1,23 @@
+import ReactMarkdown from "react-markdown";
+import matter from "gray-matter";
 import Layout from "../../../src/components/Layout";
 import styles from "./BlogPostTemplate.module.scss";
 
-export default function BlogPostTemplate() {
+export default function BlogPostTemplate(props: any) {
   return (
     <Layout>
       <ol className={styles.main}>
         <li className={styles.entry}>
-          <p className={styles.date}>January 30, 2022</p>
+          <div className={styles.number}>{props.number}.</div>
+          <p className={styles.date}>
+            {new Date(props.date).toLocaleString("default", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
           <div className={styles.entryIntro}>
-            <h2 className={styles.entryTitle}>
-              I feel like despite the shitty hand I was dealt, I did everything
-              right
-            </h2>
+            <h1 className={styles.entryTitle}>{props.quote}</h1>
             <div className={styles.entryHeading}>
               <div className={styles.entryPic}>
                 <img
@@ -41,62 +47,58 @@ export default function BlogPostTemplate() {
               </ul>
             </div>
           </div>
-          <h3 className={styles.entrySubtitle}>My Story</h3>
-          <p className={styles.entryCopy}>
-            My birth parents were addicts/abusive and I was in and out of foster
-            care. Cue miscellaneous trauma. Come high school it was time to
-            decide to go to college or not and I was told by teachers and
-            councilors to try community college as my grades weren’t the best.
-            No one recognized that I needed to go to university because once I
-            graduated high school, I was out of the foster care system and had
-            nowhere to live. I went to state school for two years amassing
-            $30,000 in debt, dropped out, and started working full time.
-          </p>
-          <p className={styles.entryCopy}>
-            I’m “one of the lucky ones” because I worked my way up into
-            management and was making six figures. My boyfriend (now husband) is
-            a unioned electrician with no student loan debt. My boyfriend and I
-            got a house (for 30k in forclosure in 2012, had it paid off by
-            2017), got married, I paid off my student loans in full, and had two
-            babies. I couldn’t continue working 10+ hour days and be away from
-            my kids that long. Also, even with my salary, daycare for two kids
-            would mean I was bringing home only one-third of my check all to
-            only get to spend MAYBE one hour a day with my kids. It wasn’t worth
-            it so I left to stay home with them.
-          </p>
-          <p className={styles.entryCopy}>
-            We’ve now outgrown our home and are ready to buy a new one, even
-            with a preapproval letter, we’re losing all the houses we’re
-            interested in to all cash offers. Our saving grace right now is that
-            we’re mortgage free and will be able to sell the home for 5x what we
-            paid for it. We’re considering moving into our Mother-in-law's
-            basement (two toddlers, two adults, and a dog) so that we can sell
-            our home and potentially compete with these cash offers.
-          </p>
-          <p className={styles.entryCopy}>
-            I feel like despite the shitty hand I was dealt, I did everything
-            right. I overcame my personal traumas and what was expected of
-            foster kids. I got the good job, worked the long hours to earn a
-            good salary. Bought a house, lived below our means to get it paid
-            off. Paid off my loans like I was supposed to. And still, I’m now
-            jobless with two small kids, and considering moving into my MIL's
-            basement. I genuinely don’t understand how people are supposed to
-            succeed.
-          </p>
-          <h3 className={styles.entrySubtitle}>Where were you on 9/11?</h3>
-          <p className={styles.entryCopy}>
-            I was in the 3rd grade coloring some American Flag worksheets.
-          </p>
-          <h3 className={styles.entrySubtitle}>
-            How did the 2008 Recession affect you?
-          </h3>
-          <p className={styles.entryCopy}>
-            I was in High School, my best friend's mom lost her job and they
-            took me in through foster care to help me which also allowed me to
-            keep the extra income.
-          </p>
+          <div className={styles.content}>
+            <ReactMarkdown>{props.content}</ReactMarkdown>
+          </div>
         </li>
       </ol>
     </Layout>
   );
+}
+
+export async function getStaticProps({ ...ctx }) {
+  const fs = require("fs");
+  // get specific markdown blog post
+  const { number, slug } = ctx.params;
+  const markdownFilePath = `./posts/${number}-${slug}.md`;
+
+  const fileContents = fs.readFileSync(markdownFilePath, "utf8");
+
+  const data = matter(fileContents);
+
+  const {
+    data: { title, date, quote },
+    content,
+  } = data;
+  return {
+    props: {
+      title,
+      date,
+      quote,
+      content,
+      number,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const fs = require("fs");
+  const paths = fs
+    .readdirSync(`./posts`)
+    .map((path: string) => {
+      const [number, ...slugArr] = path.replace(/\.mdx?$/, "").split("-");
+      const slug = slugArr.join("-");
+      return {
+        slug,
+        number,
+      };
+    })
+    .map(({ number, slug }: { number: string; slug: string }) => ({
+      params: { number, slug },
+    }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
---
+++ posts/01-pre-rendering.md
@@ -0,0 +1,19 @@
+---
+title: "Two Forms of Pre-rendering"
+date: "2022-01-30"
+quote: "I feel like despite the shitty hand I was dealt, I did everything right"
+---
+
+## My Story
+
+My birth parents were addicts/abusive and I was in and out of foster care. Cue miscellaneous trauma. Come high school it was time to decide to go to college or not and I was told by teachers and councilors to try community college as my grades weren’t the best. No one recognized that I needed to go to university because once I graduated high school, I was out of the foster care system and had nowhere to live. I went to state school for two years amassing $30,000 in debt, dropped out, and started working full time.
+
+I’m “one of the lucky ones” because I worked my way up into management and was making six figures. My boyfriend (now husband) is a unioned electrician with no student loan debt. My boyfriend and I got a house (for 30k in forclosure in 2012, had it paid off by 2017), got married, I paid off my student loans in full, and had two babies. I couldn’t continue working 10+ hour days and be away from my kids that long. Also, even with my salary, daycare for two kids would mean I was bringing home only one-third of my check all to only get to spend MAYBE one hour a day with my kids. It wasn’t worth it so I left to stay home with them.
+
+## Where were you on 9/11?
+
+We’ve now outgrown our home and are ready to buy a new one, even with a preapproval letter, we’re losing all the houses we’re interested in to all cash offers. Our saving grace right now is that we’re mortgage free and will be able to sell the home for 5x what we paid for it. We’re considering moving into our Mother-in-law's basement (two toddlers, two adults, and a dog) so that we can sell our home and potentially compete with these cash offers.
+
+## How did the 2008 recession affect you?
+
+I feel like despite the shitty hand I was dealt, I did everything right. I overcame my personal traumas and what was expected of foster kids. I got the good job, worked the long hours to earn a good salary. Bought a house, lived below our means to get it paid off. Paid off my loans like I was supposed to. And still, I’m now jobless with two small kids, and considering moving into my MIL's basement. I genuinely don’t understand how people are supposed to succeed.
